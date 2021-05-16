094a326837aae4ce9125746564d1cd4cdf6002fd
Karol Kozikowski <kozikowskik@gmail.com>
2021-05-16 13:56:35 +0200

    Add budgets app models to admin. Remove unused admin model from categories app.

--- backend/budgets/admin.py
+++ backend/budgets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from budgets.models import Budget, Expense, Income
+
 # Register your models here.
+admin.site.register(Budget)
+admin.site.register(Income)
+admin.site.register(Expense)
--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from categories.models import Category
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category)
