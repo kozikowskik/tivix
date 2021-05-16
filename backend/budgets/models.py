1209f2dc7d2925604ca1c5100f891df928f74712
Karol Kozikowski <kozikowskik@gmail.com>
2021-05-16 13:38:58 +0200

    Add categories app.

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Budget(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User)
+
+
+class Income(models.Model):
+    budget = models.ForeignKey(Budget)
+
+
+class Expense(models.Model):
+    budget = models.ForeignKey(Budget)
---
+++ backend/categories/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    class Type(models.TextChoices):
+        INCOME = "income", _("Income")
+        EXPENSE = "expense", _("Expense")
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=7, choices=Type.choices, default=Type.INCOME)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
