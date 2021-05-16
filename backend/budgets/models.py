0ecadf6cff9166d0f01f3e1bb13bfa160001649a
Karol Kozikowski <kozikowskik@gmail.com>
2021-05-16 14:14:58 +0200

    Add field shared_with to model Budget.

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -8,6 +8,8 @@ class Budget(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    shared_with = models.ManyToManyField(User, related_name="shared_with_users")
+
     def __str__(self):
         return self.name
 
