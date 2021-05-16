45fbbef9c9158a03144fb67045fa1b3ed28c26dc
Karol Kozikowski <kozikowskik@gmail.com>
2021-05-16 13:53:06 +0200

    Add Budget, Icome, Expense models to budget app.

--- backend/budgets/models.py
+++ backend/budgets/models.py
@@ -1,15 +1,23 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+from categories.models import Category
 
 
 class Budget(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Income(models.Model):
-    budget = models.ForeignKey(Budget)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Expense(models.Model):
-    budget = models.ForeignKey(Budget)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
