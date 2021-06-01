b6873e41aba2621b075b72bbdc7481e2c634c6ef
Karol Kozikowski <kozikowskik@gmail.com>
2021-06-01 19:40:12 +0200

    Refactor categories model class.

--- frontend/src/components/categories/CategoryModel.js
+++ frontend/src/components/categories/CategoryModel.js
@@ -1,43 +1,7 @@
-import API from "../../api.js";
+import BaseModel from "../BaseModel.js";
 
-export default class CategoryModel {
+export default class CategoryModel extends BaseModel {
     constructor(url) {
-        this.url = "/api/categories";
-        if (url) {
-            this.url = url;
-        }
-        this.all = this.all.bind(this);
-    }
-
-    all(data = {}, success = null, error = null) {
-        API.get(this.url, data)
-            .then((res) => {
-                if (!(typeof success === "function")) {
-                    return false;
-                }
-                success.apply(null, [res]);
-            })
-            .catch((err) => {
-                if (!(typeof error === "function")) {
-                    return false;
-                }
-                error.apply(null, error);
-            });
-    }
-
-    delete(id, success = null, error = null) {
-        API.delete(`${this.url}/${id}`)
-            .then((res) => {
-                if (!(typeof success === "function")) {
-                    return false;
-                }
-                success.apply(null, [res, this]);
-            })
-            .catch((err) => {
-                if (!(typeof error === "function")) {
-                    return false;
-                }
-                error.apply(null, error);
-            });
+        super("/api/categories");
     }
 }
