8cd8ad3476997da5a709c050e7210076b4800346
Karol Kozikowski <kozikowskik@gmail.com>
2021-06-01 16:16:28 +0200

    Add BaseModel.js to frontend app to extract same part of getting data for all models.

---
+++ frontend/src/components/BaseModel.js
@@ -0,0 +1,43 @@
+import API from "../api.js";
+
+export default class BaseModel {
+    constructor(url) {
+        if (url) {
+            this.url = url;
+        }
+        this.all = this.all.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+
+    all(data = {}, success = null, error = null) {
+        API.get(this.url, data)
+            .then((res) => {
+                if (!(typeof success === "function")) {
+                    return false;
+                }
+                success.apply(null, [res]);
+            })
+            .catch((err) => {
+                if (!(typeof error === "function")) {
+                    return false;
+                }
+                error.apply(null, error);
+            });
+    }
+
+    delete(id, success = null, error = null) {
+        API.delete(`${this.url}/${id}`)
+            .then((res) => {
+                if (!(typeof success === "function")) {
+                    return false;
+                }
+                success.apply(null, [res, this]);
+            })
+            .catch((err) => {
+                if (!(typeof error === "function")) {
+                    return false;
+                }
+                error.apply(null, error);
+            });
+    }
+}
